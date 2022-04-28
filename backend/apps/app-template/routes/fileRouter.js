56ce7b3faa08e40b33af41e347b75565d4ef2535
Ubuntu <ubuntu@ip-172-31-22-72.ap-southeast-1.compute.internal>
2022-04-28 07:37:44 +0000

    wip

--- backend/apps/app-template/routes/fileRouter.js
+++ backend/apps/app-template/routes/fileRouter.js
@@ -166,7 +166,7 @@ function cleanData(data) {
           }
 
           let numOfWords = countWords(eachReview[3]);
-          if (numOfWords > 5) {
+          if (numOfWords <= 4) {
             reviewObj = {
               [data[0][0]]: eachReview[0],
               [data[0][1]]: eachReview[1],
@@ -250,5 +250,5 @@ module.exports = express
         return res.json(output);
       },
     });
-    await fs_extra.remove('./uploadedFiles/');
+    //await fs_extra.remove('./uploadedFiles/');
   });
